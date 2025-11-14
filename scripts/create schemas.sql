5d5d27d2aca727851faed76ce2597ab91461fe59
Vijaya Supreetha Gurrala <supreetha@Supreethas-MacBook-Pro.local>
2025-11-13 17:28:49 -0800

    new schemas created

---
+++ scripts/create schemas.sql
@@ -0,0 +1,13 @@
+/* Create Database Schema
+ 
+ script purpose: 
+       
+  The script sets up three schemas within the 'datawarehouse' database named:'bronze','silver' and 'gold'.
+
+
+*/
+
+
+create schema bronze;
+create schema silver;
+create schema gold;
